8df8b5a9a13997467770ba0598b24b2d89fbb81d
Jan Trzcinski <poljant@post.pl>
2022-05-06 22:14:25 +0200

    start

---
+++ src/GateAutomation.cpp
@@ -0,0 +1,66 @@
+/*
+ * GateAutomation.cpp
+ *
+ *  Created on: 29 kwi 2022
+ *      Author: jant
+ */
+
+#include <GateAutomation.h>
+
+GateAutomation::GateAutomation() {
+	// TODO Auto-generated constructor stub
+
+}
+
+GateAutomation::~GateAutomation() {
+	// TODO Auto-generated destructor stub
+}
+
+void GateAutomation::begin() {
+
+}
+void GateAutomation::opengate(){
+
+}
+void GateAutomation::closegate(){
+
+}
+void GateAutomation::opengateA(){
+
+}
+void GateAutomation::closegateA(){
+
+}
+void GateAutomation::opengateB(){
+
+}
+void GateAutomation::closegateB(){
+
+}
+bool GateAutomation::bellon(){
+	return false;
+}
+bool GateAutomation::stategate(){
+	return false;
+}
+bool GateAutomation::stategateA(){
+	return false;
+}
+bool GateAutomation::stategateB(){
+	return false;
+}
+bool GateAutomation::statebutton(){
+	return false;
+}
+void GateAutomation::start(){
+
+}
+void GateAutomation::pause(){
+
+}
+void GateAutomation::stop(){
+
+}
+bool GateAutomation::wicketopen(){
+	return false;
+}
